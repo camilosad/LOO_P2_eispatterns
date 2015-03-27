dc61cad4f9fddd4514351c638bdce5c771b301f5
Camilo Sad <camilosad@yahoo.com.br>
2015-03-27 18:36:53 -0300

    alteração de rules

--- bank_system/rules/locadora_system_rule_base.py
+++ bank_system/rules/locadora_system_rule_base.py
@@ -3,37 +3,37 @@ from domain.supportive.rule import rule
 from domain.supportive.core_rules import CoreRules
 
 
-class BankSystemRuleBase(CoreRules):
+class LocadoraRuleBase(CoreRules):
     @rule('association')
-    def should_be_instance_of_bank_account(self, associated):
-        '''Associated object should be instance of Bank Account Decorator'''
-        from bank_system.decorators.bank_account_decorator import BankAccountDecorator
-        try: associated |should| be_instance_of(BankAccountDecorator)
+    def should_be_instance_of_movie(self, associated):
+        '''Associated object should be instance of Movie Decorator'''
+        from locadora.decorators.movie_decorator import MovieDecorator
+        try: associated |should| be_instance_of(MovieDecorator)
         except ShouldNotSatisfied: return False
         else: return True
 
     @rule('association')
-    def should_be_instance_of_credit_analyst(self, associated):
-        '''Associated object should be instance of Credit Analyst Decorator'''
-        from bank_system.decorators.credit_analyst_decorator import CreditAnalystDecorator
-        try: associated |should| be_instance_of(CreditAnalystDecorator)
+    def should_be_instance_of_attendant(self, associated):
+        '''Associated object should be instance of Attendant Decorator'''
+        from locadora.decorators.attendant_decorator import AttendantDecorator
+        try: associated |should| be_instance_of(AttendantDecorator)
         except ShouldNotSatisfied: return False
         else: return True
 
     @rule('association')
-    def should_be_instance_of_loan_request(self, associated):
-        '''Associated object should be instance of Loan Request'''
-        from bank_system.resources.loan_request import LoanRequest
-        try: associated |should| be_instance_of(LoanRequest)
+    def should_be_instance_of_rent_request(self, associated):
+        '''Associated object should be instance of Rent Request'''
+        from locadora.resources.rent_request import RentRequest
+        try: associated |should| be_instance_of(RentRequest)
         except ShouldNotSatisfied: return False
         else: return True
 
     @rule('association')
-    def should_have_employee_decorator(self, associated):
-        '''Associated object should be previously decorated by Employee'''
-        from bank_system.decorators.employee_decorator import EmployeeDecorator
+    def should_have_client_decorator(self, associated):
+        '''Associated object should be previously decorated by Client'''
+        from locadora.decorators.client_decorator import ClienteDecorator
         import domain.supportive.contract_matchers
-        try: associated |should| be_decorated_by(EmployeeDecorator)
+        try: associated |should| be_decorated_by(ClienteDecorator)
         except ShouldNotSatisfied: return False
         else: return True
 
